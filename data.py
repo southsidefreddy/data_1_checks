e11ccb9d14da6f2f807f3eb11c289d0e32326750
Fredrick Wethington <fwethington0001@gmail.com>
2023-02-07 17:17:59 -0500

    added data.py file

---
+++ data.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+list = [0, 1, 2, 3, 'four', 'five']
+print(3)
+
+pets = {'dog' : 'lucky', 'cat' : 'Jody'}
+print(pets['dog'])
+
+tuple1 = ("zero", "one", "two")
+print(tuple1)
+
